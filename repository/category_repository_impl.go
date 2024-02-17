f26d0377b1ffbc4af7893af9daee7f97744776fc
akbar-human <akbar@hub9.biz>
2024-02-17 09:51:19 +0700

    handle category respository implementation

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -1,10 +1,11 @@
 package repository
 
 import (
-	// "4kbr/restful-golang/helper"
+	"4kbr/restful-golang/helper"
 	"4kbr/restful-golang/model/domain"
 	"context"
 	"database/sql"
+	"errors"
 )
 
 type CategoryRepositoryImpl struct {
@@ -13,53 +14,58 @@ type CategoryRepositoryImpl struct {
 // repostitory *CategoryRepositoryImpl repository.CategoryRepository
 // f *File repository.CategoryRepository
 func (f *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	panic("not implemented") // TODO: Implement
+	SQL := "insert into customer(name) values (?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
 }
 
 func (f *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	panic("not implemented") // TODO: Implement
+	SQL := "update category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfError(err)
+
+	return category
 }
 
 func (f *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
-	panic("not implemented") // TODO: Implement
+	SQL := "update category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfError(err)
 }
 
-func (f *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category {
-	panic("not implemented") // TODO: Implement
+func (f *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "select id, name from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
 }
 
 func (f *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
-	panic("not implemented") // TODO: Implement
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+
+		categories = append(categories, category)
+	}
+	return categories
 }
-
-//c *CategoryRepositoryImpl package.domain.Category
-
-//repository CategoryRepositoryImpl
-
-// func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-// 	SQL := "insert into customer(name) values (?)"
-// 	result, err := tx.ExecContext(ctx, SQL, category.Name)
-// 	helper.PanicIfError(err)
-
-// 	id, err := result.LastInsertId()
-// 	helper.PanicIfError(err)
-
-// 	category.Id = int(id)
-// 	return category
-// }
-
-// func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-// 	panic("not implemented") // TODO: Implement
-// }
-
-// func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
-// 	panic("not implemented") // TODO: Implement
-// }
-
-// func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category {
-// 	panic("not implemented") // TODO: Implement
-// }
-
-// func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
-// 	panic("not implemented") // TODO: Implement
-// }
